d35e0cad38ff6c6483d2907a93447f5cfab658ab
vino-brutor <viktorbruno2006@gmail.com>
2024-04-18 12:35:13 -0300

    first commit

---
+++ enuns.ts
@@ -0,0 +1,18 @@
+enum Planets {
+    MERCURIO = 'mercurio',
+    VENUS = 'venus',
+    TERRA = 'terra',
+    MARTE = 'marte'
+}
+//vai retoner a string
+//por padrão ele vai começar por zero e ir aidicionando
+
+
+Planets.TERRA
+
+enum Roles{
+    ADMIN,
+    USER
+}
+
+Roles.ADMIN //vai retornar zero
